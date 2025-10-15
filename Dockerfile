9b09bd30a013d00b4f48f0b3e57c8bae82e6c9d7
aniket <aniket@9766228627@gmail.com>
2025-10-15 10:20:38 +0530

    Updated version

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ FROM openjdk:17-jdk-slim
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
 ENV JAVA_OPTS=""
-ENV EUREKA_SERVER_URL=http://eureka-server:8761/eureka/
+#ENV EUREKA_SERVER_URL=http://eureka-server:8761/eureka/
 EXPOSE 8080
 ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -jar app.jar"]
 
--- src/main/java/com/Aniket/API_Gateway/ApiGatewayApplication.java
+++ src/main/java/com/Aniket/API_Gateway/ApiGatewayApplication.java
@@ -3,10 +3,10 @@ package com.Aniket.API_Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
